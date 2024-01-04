1ed20a88bd1b27232bfc1cb4a7192ddae117c787
Mehdi_ha01@itstep.edu.az <Mehdi_ha01@itstep.edu.az>
2024-01-04 20:06:13 +0400

    Add project files.

---
+++ ProductsAsp/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductsAsp.Models;
+
+namespace ProductsAsp.Controllers
+{
+    public class ProductController : Controller
+    {
+        static List<Product> products = new() {
+
+            new Product { ProductName ="Colla", Description = "harmful",Price=2.50},
+            new Product{ ProductName = "Fanta", Description = "harmful",Price=2.20},
+            new Product{ ProductName = "Cappy", Description = "harmful",Price=3},
+            new Product{ ProductName = "Badamli", Description = "harmful",Price=2.60},
+            new Product{ ProductName = "Sirab", Description = "harmful",Price=1},
+            new Product{ ProductName = "Red Bull", Description = "harmful",Price=4.50},
+            new Product{ ProductName = "Hell", Description = "harmful",Price=3.70},
+        };
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            var product=products.FirstOrDefault(p=>p.Id==id);
+            return View(product);
+        }
+    }
+}
---
+++ ProductsAsp/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductsAsp.Models
+{
+    public class Product
+    {
+        static int _staticId = 0;
+        public int Id { get; set; } = ++_staticId;
+        public string ProductName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public double Price { get; set; }
+    }
+}
